b8d5a9455884a7da3a168de0e4e0ec3e2c3f789a
ZdenekSrotyr <zdenek.srotyr@keboola.com>
2024-10-11 23:01:33 +0200

    add scope

--- src/client/client.py
+++ src/client/client.py
@@ -79,7 +79,8 @@ class OneDriveClient(HttpClient):
         client_type = "OneDrive"
         authority = 'https://login.microsoftonline.com/common'
         self.base_url = 'https://graph.microsoft.com/v1.0/me'
-        scope = ['https://graph.microsoft.com/User.Read', 'https://graph.microsoft.com/Files.Read.All']
+        scope = ['https://graph.microsoft.com/User.Read', 'https://graph.microsoft.com/Files.Read.All',
+                 'https://graph.microsoft.com/wl.offline_access']
         return client_type, authority, scope
 
     def _configure_sharepoint_client(self):
